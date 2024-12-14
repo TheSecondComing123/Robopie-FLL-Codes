1e832ee0a817501246df09298bcdf8335fc337cd
TheSecondComing123 <78659211+TheSecondComing123@users.noreply.github.com>
2024-12-14 15:50:29 -0500

    Update run_6.py

--- run_6.py
+++ run_6.py
@@ -1,5 +1,5 @@
 def run_6():
-    #Aligner two spaces right of left corner
+    # Aligner two spaces right of left corner
     run_lift_arm(300)
     forward(150)
     turn(-70)
@@ -8,23 +8,23 @@ def run_6():
     forward(620)
     turn(115)
     forward(55)
-    #Shark
+    # Shark
     run_lift_arm(-320, 1660)
     run_lift_arm(250)
     forward(-150)
     turn(-238)
-    #Coral Nursery
+    # Coral Nursery
     forward(-200)
     run_lift_arm(-100)
     turn(130)
     forward(210)
     turn(-60)
-    #Coral Habitat
+    # Coral Habitat
     run_lift_arm(-300, 660)
     forward(25)
     run_lift_arm(-25, 660)
     forward(-60)
-    #Angler Fish
+    # Angler Fish
     run_lift_arm(290)
     turn(-90)
     forward(500)
