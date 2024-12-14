58433ee8c66b8189bb560845f84e79ec3edd2ee8
beneben <bchen5844@gmail.com>
2024-12-14 16:25:57 -0500

    Update run_6.py

--- run_6.py
+++ run_6.py
@@ -1,5 +1,5 @@
 def run_6():
-    # Aligner two spaces right of left corner
+    #Aligner two spaces right of corner
     run_lift_arm(300)
     forward(150)
     turn(-70)
@@ -7,28 +7,30 @@ def run_6():
     turn(45)
     forward(620)
     turn(115)
-    forward(55)
-    # Shark
+    forward(58)
+    #Shark
     run_lift_arm(-320, 1660)
-    run_lift_arm(250)
+    run_lift_arm(240)
     forward(-150)
     turn(-238)
-    # Coral Nursery
+    #Coral Nursery
     forward(-200)
     run_lift_arm(-100)
     turn(130)
-    forward(210)
-    turn(-60)
-    # Coral Habitat
+    forward(200)
+    turn(-65)
+    #Coral Habitat
     run_lift_arm(-300, 660)
-    forward(25)
+    forward(20)
     run_lift_arm(-25, 660)
     forward(-60)
-    # Angler Fish
+    #Angler Fish
     run_lift_arm(290)
-    turn(-90)
-    forward(500)
+    turn(-95)
+    forward(325)
+    turn(60)
+    forward(150)
     run_lift_arm(-305, 660)
-    turn(75)
-    forward(175)
-    turn(-230)
+    turn(-40)
+    forward(170)
+    turn(-250)
