1cdcd184254703a9a22cf674f044a31613980096
beneben <bchen5844@gmail.com>
2024-12-22 17:35:51 -0500

    Update krakens_treasure.py

--- krakens_treasure.py
+++ krakens_treasure.py
@@ -1,11 +1,11 @@
 def krakens_treasure():
     #Aligner four spaces right of corner
     forward(100)
-    turn(-75)
-    forward(170)
+    turn(-80)
+    forward(200)
     turn(70)
-    forward(380)
-    turn(-150)
-    forward(300)
+    forward(335)
+    turn(-141)
+    forward(305)
     time.sleep(1)
     forward(-250,120)
