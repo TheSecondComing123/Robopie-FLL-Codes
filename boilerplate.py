6432046fec9e34d27774475b8c65e3afd7decafc
TheSecondComing123 <78659211+TheSecondComing123@users.noreply.github.com>
2024-12-22 20:22:12 -0500

    Update boilerplate.py

--- boilerplate.py
+++ boilerplate.py
@@ -8,20 +8,20 @@ razor_blade_port = port.C
 left_motor_port = port.A
 right_motor_port = port.E
 
-def forward(degrees, speed=360): # postitive = forward, negative = backward
+def forward(degrees, speed=360, *, delay=0.25): # postitive = forward, negative = backward
     motor.run_for_degrees(left_motor_port, -degrees, speed)
     motor.run_for_degrees(right_motor_port, degrees, speed)
-    time.sleep(abs(degrees) / speed + 0.25)
+    time.sleep(abs(degrees) / speed + delay)
 
-def turn(degrees, speed=360): # positive = left, negative = right
+def turn(degrees, speed=360, *, delay=0.25): # positive = left, negative = right
     degrees = int(degrees)
     motor.run_for_degrees(left_motor_port, degrees, speed)
     motor.run_for_degrees(right_motor_port, degrees, speed)
-    time.sleep(abs(degrees) / speed + 0.25)
+    time.sleep(abs(degrees) / speed + delay)
 
-def run_lift_arm(degrees, speed=360): # positive = up, negative = down
+def run_lift_arm(degrees, speed=360, *, delay=0.25): # positive = up, negative = down
     motor.run_for_degrees(lift_arm_port, degrees, speed)
-    time.sleep(abs(degrees) / speed + 0.25)
+    time.sleep(abs(degrees) / speed + delay)
 
 def reset_lift_arm_port(): 
     motor.run_for_degrees(lift_arm_port, 300, 360) 
