bb5d785cdceb5b2d8566ca6271c99c88e52d73fc
beneben <bchen5844@gmail.com>
2024-12-28 15:24:55 -0500

    Update run_6.py

--- run_6.py
+++ run_6.py
@@ -1,13 +1,12 @@
 def run6():
     # Aligner two spaces right of corner
-    run_lift_arm(300)
     forward(150)
     turn(-70)
     forward(170)
     turn(45)
     forward(620)
-    turn(115)
-    forward(58)
+    turn(125)
+    forward(110)
     # Shark
     run_lift_arm(-320, 1660)
     run_lift_arm(240)
@@ -29,8 +28,17 @@ def run6():
     turn(-95)
     forward(300)
     turn(60)
-    forward(160)
+    forward(190)
     run_lift_arm(-305, 660)
-    turn(-40)
-    forward(170)
+    turn(-60)
+    forward(200)
     turn(-250)
+    #Back to home base
+    forward(-100)
+    turn(-90)
+    forward(100)
+    turn(-50)
+    forward(250)
+    turn(60)
+    forward(330)
+    
