786eea46beecc28c95b97e4dde64351068d2d220
beneben <bchen5844@gmail.com>
2025-01-12 17:30:00 -0500

    Create feed_whale.py

---
+++ feed_whale.py
@@ -0,0 +1,12 @@
+def feed_whale():
+    #Dumps the krill in the whale
+    forward(270)
+    turn(90, 180)
+    forward(360, 270)
+    turn(-30, 180)
+    forward(160, 180)
+    turn(-60, 180)
+    forward(175 , 180)
+    turn(-75, 180)
+    forward(185, 170)
+    forward(-150, 150)
