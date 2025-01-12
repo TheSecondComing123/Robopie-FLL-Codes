f1f62fb08d4d8ac3b43199fdfa18e05281f08c4d
beneben <bchen5844@gmail.com>
2025-01-12 17:37:04 -0500

    Update unspecified_run.py

--- unspecified_run.py
+++ unspecified_run.py
@@ -1,17 +1,3 @@
-def run4b():
-    #IDK WHAT THIS DOES TO TEST
-    forward(-100, 180)
-    turn(-20, 180)
-    forward(165, 180)
-    forward(-180)
-    turn(100, 180)
-    forward(-750, 180)
-    turn(45, 180)
-    forward(-130, 205)
-
-    # Back to home base
-    turn(90, 100)
-    forward(-840, 180)
 def run4c2():
     #Not known in this time situation.
     forward(40)
