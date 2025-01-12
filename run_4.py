cbeac04f5628835ba680a552d47a7caf027a2295
beneben <bchen5844@gmail.com>
2025-01-12 17:29:32 -0500

    Update run_4.py

--- run_4.py
+++ run_4.py
@@ -1,17 +1,3 @@
-
-def run4a():
-    #Dumps the krill in the whale
-    forward(270)
-    turn(90, 180)
-    forward(360, 270)
-    turn(-30, 180)
-    forward(160, 180)
-    turn(-60, 180)
-    forward(175 , 180)
-    turn(-75, 180)
-    forward(185, 170)
-    forward(-150, 150)
-
 def run4b():
     #Get hoop while comes back
     forward(-100, 180)
