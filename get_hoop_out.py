57c3a2b6d1270371a3b0860d28ffd049f0b139ea
beneben <bchen5844@gmail.com>
2025-01-12 17:37:36 -0500

    Create get_hoop_out.py

---
+++ get_hoop_out.py
@@ -0,0 +1,14 @@
+def get_hoop_out():
+    #IDK WHAT THIS DOES TO TEST
+    forward(-100, 180)
+    turn(-20, 180)
+    forward(165, 180)
+    forward(-180)
+    turn(100, 180)
+    forward(-750, 180)
+    turn(45, 180)
+    forward(-130, 205)
+
+    # Back to home base
+    turn(90, 100)
+    forward(-840, 180)
