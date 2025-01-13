c66c70c498b3ca275fbe7dbfc8ddd25b9a6fd4f7
beneben <bchen5844@gmail.com>
2025-01-12 21:05:58 -0500

    Update feed_whale.py

--- feed_whale.py
+++ feed_whale.py
@@ -1,11 +1,12 @@
-def run3():
-    forward(180)
-    turn(90, 180)
-    forward(340, 270)
-    turn(-3, 180)
-    forward(200, 180)
-    turn(-70, 180)
-    forward(230, 180)
-    turn(-80, 180)
-    forward(200, 180)
-    forward(-90, 180)
+def run4a2(): 
+    #dumps the krill in the whale 
+    forward(270) 
+    turn(90, 180) 
+    forward(280, 270) 
+    turn(-20, 180) 
+    forward(164, 180) 
+    turn(-50, 180)
+    forward(200 , 180) 
+    turn(-80, 180) 
+    forward(400, 170) 
+    forward(-150, 150)
