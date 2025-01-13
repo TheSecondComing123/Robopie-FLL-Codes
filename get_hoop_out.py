8e81de9244a877121da392142096e2c8ca94e955
beneben <bchen5844@gmail.com>
2025-01-12 19:05:09 -0500

    Update get_hoop_out.py

--- get_hoop_out.py
+++ get_hoop_out.py
@@ -1,13 +1,12 @@
-def get_hoop_out():
-    #IDK WHAT THIS DOES TO TEST
-    forward(-100, 180)
-    turn(-20, 180)
-    forward(165, 180)
-    forward(-180)
-    turn(100, 180)
-    forward(-750, 180)
-    turn(45, 180)
-    forward(-130, 205)
+def run4b():
+    # #Get hoop while comes back
+    # forward(-100, 180)
+    # turn(-20, 180)
+    # forward(225, 180)
+    # forward(-180)
+    # turn(100, 180)
+    # forward(-750, 180)
+
 
     # Back to home base
     turn(90, 100)
