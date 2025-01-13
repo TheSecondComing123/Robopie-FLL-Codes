dfd2c4a34a85ef60abab4853f4e9acdb2bc49c44
beneben <bchen5844@gmail.com>
2025-01-12 21:03:39 -0500

    Update get_hoop_out.py

--- get_hoop_out.py
+++ get_hoop_out.py
@@ -1,11 +1,11 @@
 def run4b():
-    # #Get hoop while comes back
-    # forward(-100, 180)
-    # turn(-20, 180)
-    # forward(225, 180)
-    # forward(-180)
-    # turn(100, 180)
-    # forward(-750, 180)
+    #Get hoop while comes back
+    forward(-100, 180)
+    turn(-20, 180)
+    forward(225, 180)
+    forward(-180)
+    turn(100, 180)
+    forward(-750, 180)
 
 
     # Back to home base
