74764749ba410cc0313342adab0b68b6d511e349
beneben <bchen5844@gmail.com>
2025-01-18 12:56:01 -0500

    Update and rename get_hoop_out.py to get_hoop.py

---
+++ get_hoop.py
@@ -0,0 +1,12 @@
+def run4b():
+    forward(-100, 180)
+    turn(-20, 180)
+    forward(225, 180)
+    forward(-180)
+    turn(150, 180)
+    turn(-30)
+    forward(-750, 180)
+
+    # Back to home base
+    turn(90, 100)
+    forward(-840, 180)
